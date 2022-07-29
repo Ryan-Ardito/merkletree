74d6ca68adea17d325a487d3ff8e501f0a2f00f9
Ryan Ardito <ryan.m.ardito@gmail.com>
2022-07-28 20:27:17 -0600

    added insert

--- src/mtree.rs
+++ src/mtree.rs
@@ -34,7 +34,7 @@ fn build_merkletree(mut leaves: Vec<MerkleHash>) -> Vec<Vec<MerkleHash>> {
     // build layers up to root
     while layers.last().unwrap().len() > 1 {
         let mut layer: Vec<MerkleHash> = Vec::new();
-        // iterate over hashes in pairs to generate parents
+        // iterate over hashes in pairs to generate parent hash
         for i in (0..layers.last().unwrap().len()).step_by(2) {
             let node = _hash(
                 [
@@ -66,8 +66,10 @@ fn pad_layer(layer: &mut Vec<MerkleHash>) {
     for _ in 0..(target_len - layer.len()) {
         layer.push(layer.last().unwrap().clone());
     }
+    assert!(layer.len() & (layer.len() - 1) == 0);
 }
 
+/// Build merkle trees, get proofs, and verify proofs from hashabe data
 pub struct MerkleTree {
     layers: Vec<Vec<MerkleHash>>,
 }
@@ -90,6 +92,13 @@ impl MerkleTree {
         self.layers.last().unwrap()[0]
     }
 
+    pub fn insert<T: Hash>(&mut self, data: T) {
+        let hash = _hash(data);
+        self.insert_hash(hash);
+    }
+
+    /// Generates a merkle proof from hashable data.
+    /// Return Err if hash of data not in tree
     pub fn gen_proof<T: Hash>(&self, element: T) -> Result<MerkleProof, &str> {
         let hash = _hash(element);
         self.proof(hash)
@@ -118,6 +127,37 @@ impl MerkleTree {
         }
 
         Ok(proof)
+    }
+
+    fn insert_hash(&mut self, hash: MerkleHash) {
+        let leaves = &mut self.layers[0];
+        for i in 1..leaves.len() {
+            if leaves[i-1] == leaves[i] {
+                leaves[i] = hash;
+                self.recalculate_branch(i);
+                return
+            }
+        }
+        self.layers[0].push(hash);
+        *self = MerkleTree::from_leaves(self.layers[0].to_owned());
+    }
+
+    fn recalculate_branch(&mut self, leaf_idx: usize) {
+        let mut node_idx = leaf_idx;
+        let mut layer_idx = 0;
+        for _ in 0..self.layers.len() - 1 {
+            let layer = &self.layers[layer_idx];
+            let parent_idx = node_idx / 2;
+            // determine if changed node is left or right child
+            let (left, right) = match node_idx % 2 == 0 {
+                true => (layer[node_idx], layer[node_idx + 1]),
+                false => (layer[node_idx - 1], layer[node_idx]),
+            };
+            // rehash parent node
+            self.layers[layer_idx + 1][parent_idx] = _hash([left, right].concat());
+            node_idx = parent_idx;
+            layer_idx += 1;
+        }
     }
 
     fn verify_hash(&self, proof: &MerkleProof, hash: MerkleHash, root: MerkleHash) -> bool {
@@ -150,6 +190,21 @@ mod tests {
     }
 
     #[test]
+    fn insert() {
+        let elements = vec!["foo", "bar"];
+        let mut tree = MerkleTree::new(&elements);
+        assert!(tree.gen_proof("baz").is_err());
+        tree.insert("baz");
+        let proof = tree.gen_proof("baz");
+        assert!(proof.is_ok());
+        assert!(tree.verify(&proof.unwrap(), "baz", tree.root()));
+        tree.insert("quox");
+        let proof = tree.gen_proof("quox");
+        assert!(proof.is_ok());
+        assert!(tree.verify(&proof.unwrap(), "quox", tree.root()));
+    }
+
+    #[test]
     fn proof() {
         let elements = vec!["foo", "bar"];
         let tree = MerkleTree::new(&elements);
@@ -169,5 +224,20 @@ mod tests {
         let proof = tree.gen_proof("bar").unwrap();
         assert!(tree.verify(&proof, "bar", root));
         assert!(!tree.verify(&proof, "baz", root))
+    }
+
+    #[test]
+    fn tree_should_be_perfect() {
+        let elements = vec!["foo", "bar", "baz"];
+        let tree = MerkleTree::new(&elements);
+
+        let tree_height = tree.layers.len();
+        let leaves = &tree.layers[0];
+        // assert that leaves.len() is 2 ^ higher layers
+        assert_eq!(leaves.len(), 1 << (tree_height - 1));
+        // assert that all layers are half the length of their childrens' layers
+        for i in 0..(tree_height - 1) {
+            assert_eq!(tree.layers[i].len(), tree.layers[i + 1].len() * 2);
+        }
     }
 }
